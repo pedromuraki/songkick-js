20f0d2ef001d876eff7b7b8be42a1f99969f0759
Pedro Muraki <pedromuraki@gmail.com>
2018-10-17 18:15:58 -0300

    Update gitignore

--- examples/app.js
+++ examples/app.js
@@ -2,15 +2,17 @@ import SongkickJS from '../dist/SongkickJS.min.js';
 
 const songkick = new SongkickJS('KwvChI9jW9D3aYSd');
 
-songkick.getUserTrackings({
-  username: 'pedromuraki',
-  trackingObject: 'artists',
-  optionalParams: {
-    page: 1,
-    per_page: 25,
-    fields: 'displayName',
-    created_after: '2018-02-28T13:37:00Z',
-  },
-}).then(data => {
-  console.log(data);
-});
+songkick
+  .getUserTrackings({
+    username: 'pedromuraki',
+    trackingObject: 'artists',
+    optionalParams: {
+      page: 1,
+      per_page: 25,
+      fields: 'displayName',
+      created_after: '2018-02-28T13:37:00Z'
+    }
+  })
+  .then(data => {
+    console.log(data);
+  });
